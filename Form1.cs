c56e974ee410c1c93644e2a6a49e0973bf5452c1
Skuukzky-dev <medalcsgo@gmail.com>
2025-04-07 15:18:02 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using Npgsql;
+using TRNS.Core.DAL;
+
+namespace PostgreSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                TRNS.Core.BO.cUser oUser = new TRNS.Core.BO.cUser();
+
+                oUser.userName = "MILTON";
+                oUser.password = "123456";
+                oUser.tipoUsuarioID = 1;
+
+                TRNS.Core.BLL.UsersMgr.Create(oUser);
+
+             //   List<TRNS.Core.BO.cUser> lstUsers = TRNS.Core.BLL.UsersMgr.GetUsers();
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
